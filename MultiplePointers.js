3e48b1b6d0c8eeae648635fee10698137a082873
benjygarrison <46270616+benjygarrison@users.noreply.github.com>
2022-03-07 16:50:15 -0800

    add: algo pattern examples

---
+++ MultiplePointers.js
@@ -0,0 +1,18 @@
+//Multiple Pointers O(n):
+
+//Set two pointers to pass through a SORTED structure. Useful for
+//finding matching or unique values.
+
+//Ex:
+function countUniqueValues(arr){
+    if(arr.length === 0) return 0;
+    var i = 0;
+    for(var j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1;
+}
+countUniqueValues([1,2,2,5,7,7,99])
